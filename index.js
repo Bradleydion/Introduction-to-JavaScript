f6a3fd2356f629f6268546c3bdd24e72acb810b2
Bradley Dion <bradley@Bradleys-MacBook-Air.local>
2020-08-23 17:21:06 -0400

    completed task 6

--- index.js
+++ index.js
@@ -121,8 +121,12 @@ kMconversion()
 // 99 bottles of soda on the wall
 // create a function called annoyingSong
 // the function should take a starting number as an argument and count down - at each iteration it should log (number) bottles of soda on the wall, (number) bottles of soda, take one down pass it around (number left over) bottles of soda on the wall`
-  
-
+let bottleA = 99;
+do {
+console.log(`${bottleA} bottles of soda on the wall, ${bottleA} bottles of soda, take one down pass it around ${bottleA-1} bottles of soda on the wall`)
+bottleA--;
+} while (bottleA > 0)
+ 
 
 
 
